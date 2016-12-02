be312597a45730bd1f228df5aef57f6df29dc80c
LinkinYoung <1143052146@qq.com>
2016-12-02 16:13:41 +0800

    静态页面、易班跳转
    静态页面完成。由易班链接可以成功跳转。

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year
+        format: 'yyyy-mm-dd'
+    });
+
+    $.getJSON("API/jumptest.php", function(data) {
+
+    });
+});
